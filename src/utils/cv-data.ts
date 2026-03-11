f37363eb1b98e3ff72edcf2c50daeae5bd2cb052
Ammar <Ammar.23120273@muc.edu.eg>
2026-03-11 03:52:42 +0200

    Update resume and portfolio URLs to ammar-yasseer.com

--- src/utils/cv-data.ts
+++ src/utils/cv-data.ts
@@ -8,8 +8,8 @@ export const cvData = {
         location: "Alexandria",
         linkedin: "linkedin.com/in/ammar-yasseer",
         github: "github.com/3mmarYasser",
-        onlineCV: "resume.ammar-yasser.me",
-        portfolio: "ammar-yasser.me",
+        onlineCV: "resume.ammar-yasseer.com",
+        portfolio: "ammar-yasseer.com",
     },
     summary:
         "Experienced with complex full stack development, combining robust back-end logic with intuitive front-end interfaces. Proven track record of architecting and delivering high-performance, scalable web applications using modern frameworks and libraries. Expert in integrating APIs, optimizing database performance, and implementing security best practices to ensure data integrity and exceptional user experience. Skilled in leading technical teams and collaborating across departments to align development efforts with business objectives.",
